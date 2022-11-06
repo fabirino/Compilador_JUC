8b0c4724d63c9ec97a6cbae9e6f6618a000d725b
Eduardo22 <figueiredo92002@gmail.com>
2022-11-06 16:09:07 +0000

    onde andas segfault 1.1

---
+++ Meta2/STree.c
@@ -0,0 +1,59 @@
+#include "STree.h"
+
+node * newNode(char * var) {
+    node * aux2 = (node *) malloc(sizeof(node));
+
+    aux2->var = var;
+    aux2->brother = NULL;
+    aux2->child = NULL;
+
+    return aux2;
+
+}
+
+void addBrother(node* old, node* newn) {
+
+    while (old->brother != NULL)
+        old = old->brother;
+
+    old->brother = newn;
+
+    return;
+}
+
+void printSTree(struct node *node, int prof){
+    char pontos [prof * 2];
+
+    for (int i = 0; i < prof * 2; i++){
+        pontos[i]='.';
+    }
+    pontos [2 * prof] = '\0';
+
+    printf("%s%s\n", pontos, node->var);
+    if (node->child)    printSTree(node->child, prof + 1);
+    if (node->brother)  printSTree(node->brother, prof);
+
+    free(node);
+}
+
+
+void freeSTree(struct node *node){
+    if (node->child)    freeSTree(node->child);
+    if (node->brother)  freeSTree(node->brother);
+
+    free(node);
+}
+
+int numBrothers(struct node * no) {
+		int count = 0;
+		struct node * aux;
+		aux = no;
+		while (aux != NULL) {
+			aux = aux->brother;
+			count++;
+		}
+		return count;
+}
+
+
+
